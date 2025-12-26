261d3de0b1c1353ab03cbb963e805a2a44157a06
Joaquin Ferreyra <joaquin.ignacio.ferreyra@mi.unc.edu.ar>
2025-12-26 02:31:21 -0300

    ADD migracion a React

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+
+function OptMap({items}){
+  return items.map((option, index) => (
+    <option key={index} value={option}>{option}</option>
+  ));
+}
 
 function App() {
+  const [carrera, setCarrera] = useState('');
+  const [Anio, setAnio] = useState('');
+  const [Cuatrimestre, setCuatrimestre] = useState('');
+  
+  const Carreras = ['Lic. en Matemática','Lic. en Astronomía','Lic. en Física',
+                    'Lic. en Cs. de la Computación','Lic. en Matemática Aplicada',
+                    'Lic. en Hidrometeorología','Prof. en Matemática',
+                    'Prof. en Física','Analista en Computación',
+                    'Tec. Un. en Matemática Aplicada','Tec. Un. en Astronomía'
+                  ];
+  const Anios = ['Primero','Segundo','Tercero','Cuarto','Quinto'];
+  const Cuatrimestres =['Primero','Segundo'];
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <h1>Horarios FAMAF</h1>
+      <div className='elements'>
+        <select value={carrera} onChange={(e) => setCarrera(e.target.value)}>
+          <option value="" disabled hidden>Selecciona una Carrera</option>
+          <OptMap items={Carreras}/>
+        </select>
+
+        <select value={Anio} onChange={(e) => setAnio(e.target.value)}>
+          <option value="" disabled hidden>Selecciona tu Anio</option>
+          <OptMap items={Anios}/>
+        </select>
+
+        <select value={Cuatrimestre} onChange={(e) => setCuatrimestre(e.target.value)}>
+          <option value="" disabled hidden>Selecciona un cuatrimestre</option>
+          <OptMap items={Cuatrimestres}/>
+        </select>
+        <button className='button'>Enviar</button>
+      </div>
     </div>
   );
 }
