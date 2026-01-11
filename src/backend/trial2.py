be905eecf4f789f584fd4f82c24be70aff99f1af
Joaquin <joaquin.ignacio.ferreyra@mi.unc.edu.ar>
2026-01-10 23:56:53 -0300

    ADD comisiones fisica

--- src/backend/trial2.py
+++ src/backend/trial2.py
@@ -8,12 +8,30 @@ YEAR = 2025
 #quinto
 #url = "https://calendar.google.com/calendar/ical/qikesifu31eutm83pj8ieg55rc@group.calendar.google.com/public/basic.ics"
 #cuarto
-url = "https://calendar.google.com/calendar/ical/te92ikk33p99erffndio7n05r4@group.calendar.google.com/public/basic.ics"
+#url = "https://calendar.google.com/calendar/ical/te92ikk33p99erffndio7n05r4@group.calendar.google.com/public/basic.ics"
+#tercero 
+url = "https://calendar.google.com/calendar/ical/fa5rbun3hjemqcdsdc7jhk2a74@group.calendar.google.com/public/basic.ics"
 reporte = []
 
 def parser_materia(input):
     parsed_res = re.findall(r"AULA \d+|SALA [A-Z]+ Y AULA \d+|AULA: \d+|LEF\d+|LEF \d+", input)
     return parsed_res
+
+def comparser(inputcom, starthour, endhour):
+    datos = []
+    print(f"input:{inputcom}")
+    for c in inputcom:
+        nombre_c = re.search(r"(?:comisión|com\.?)\s*(\d+)", c, re.IGNORECASE)
+        print(f"{nombre_c}")
+        aula = re.findall(r"aula\s*\d+|LEF\s*\d*|SALA\s*[A-Z]+", c, re.IGNORECASE)
+        if nombre_c and aula:
+            datos.append({
+                "comm": nombre_c.group(1),      # solo el número
+                "Ubicacion": aula, 
+                "Horario": f"{starthour} - {endhour}"
+            })
+    return datos
+
 def obtener_data():
 # obtenemos el url
     result = requests.get(url)
@@ -36,21 +54,36 @@ def obtener_data():
                     hora_inicio = index_date.strftime("%H:%M")
                     hora_fin = component.get('dtend').dt.strftime("%H:%M")
                     aulareal = materia_lugar.upper()
-                    # procesamos la ubicacion segun cada caso
-                    aulas = parser_materia(aulareal)
-                    
+                    # Obtenemos las comisiones de una materia
+                    datacomm = re.findall(r"(?:com\.?|comisión)\s*\d+[^/]*", materia_lugar, re.IGNORECASE)
+                    print(f"comm: {datacomm}")
+                    comisiones = []
+                    if datacomm:
+                        print("entramos a comparser")
+                        comisiones = comparser(datacomm, hora_inicio, hora_fin)
+                    else:
+                        # procesamos la ubicacion segun cada caso
+                        aulas = parser_materia(aulareal)
+                        comisiones = [{
+                            "comm":"Unica",
+                            "Ubicacion": aulas,
+                            "Horario": f"{hora_inicio}-{hora_fin}"
+                        }]
+
                     reporte.append({
                         "nombre": nombre_m,
-                        "Ubicacion":aulas,
-                        "horario": f"{hora_inicio} - {hora_fin}"
+                        "Comisiones":
+                            comisiones
                     })
 
     for r in reporte:
         print(
             f"{r['nombre']}\n"
-            f"\tUbicación:\t{r['Ubicacion']}\n"
-            f"\tHorario:\t{r['horario']}\n"
+            f"\tComisiones:"
         )
+        for c in r['Comisiones']:
+            print(f"\t\tComision: {c['comm']}, Ubicacion: {c['Ubicacion']}, Horario: {c['Horario']}")
+            
 
 
 def main():
--- src/backend/trials.py
+++ src/backend/trials.py
@@ -3,22 +3,56 @@ from icalendar import Calendar
 from datetime import datetime
 
 #url quinto anio Fisica
-url = "https://calendar.google.com/calendar/ical/qikesifu31eutm83pj8ieg55rc@group.calendar.google.com/public/basic.ics"
+#url = "https://calendar.google.com/calendar/ical/qikesifu31eutm83pj8ieg55rc@group.calendar.google.com/public/basic.ics"
+# tercero
 reporte = []
 materias_raras = [
                   "Física del Estado Sólido - AULA 16", 
                   "Física Computacional - SALA IPT y AULA 32",
                   "Procesamiento de imágenes satelitales meteorológicas con Python - AULA 14",
-                  "Agujeros Negros y Singularidades - AULA 21"
+                  "Agujeros Negros y Singularidades - AULA 21",
+                  "Electromagnetismo I - AULA 31 - Práctico Aula 10 y 16",
+                  "Física General IV (T) - AULA 27",
+                  "Fís Gral IV (P) Com.1 Aula 27/ Com.2 Aula 20"
                   ]
+def comparser(input):
+    datos = []
+    for c in input:
+        nombre_c = re.search(r"com\.(\d+)", c, re.IGNORECASE)
+        aula = re.findall(r"aula \d+", c, re.IGNORECASE)
+        if nombre_c and aula:
+            datos.append({
+                "comm": nombre_c.group(1),      # solo el número
+                "Ubicacion": aula 
+            })
+    return datos
 
 for m in materias_raras:
     nombre_m = re.split(" - ", m)[0]
-    aulareal = m.upper()
-    aulas = re.findall(r"AULA \d+|SALA [A-Z]+ Y AULA \d+", aulareal)   
+    # caso raro 1: Com.1 Aula x/ Com.2 Aula y
+    datacomm = re.findall(r"com.\d+ [A-Z]+ \d+", m, re.IGNORECASE)
+    comisiones = []
+    if datacomm:
+        comisiones = comparser(datacomm)
+    else:
+        aulas = re.findall(r"aula \d+ y \d+|aula \d+|sala [A-Z]+ y aula \d+", m, re.IGNORECASE)
+        comisiones = [{
+             "comm":"Unica",
+             "Ubicacion": aulas
+        }]
+
     reporte.append({
         "nombre": nombre_m,
-        "Ubicacion":aulas
+        "Comisiones":
+              comisiones
     })
 
-print(f"{reporte}")
+
+for r in reporte:
+        print(
+            f"{r['nombre']}\n"
+            f"\tComisiones:"
+        )
+        for c in r['Comisiones']:
+            print(f"\t\tComision: {c['comm']}, Ubicacion: {c['Ubicacion']}")
+            
